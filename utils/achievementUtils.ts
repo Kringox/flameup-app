51726f7fee0132fe47c8dff38e0cd7b7985d93b8
Robin Peters <knowya420@mail.de>
2025-10-24 13:43:08 +0200

    Big Update 1.7

--- utils/achievementUtils.ts
+++ utils/achievementUtils.ts
@@ -1,10 +1,6 @@
-// FIX: Import React to provide React namespace for React.ReactElement
 import * as React from 'react';
-// FIX: Added file extension to types import
-// FIX: Corrected import path for types from ./ to ../
-import { User, Post } from '../types.ts';
-// FIX: Added file extension to icon import
-import TrophyIcon from '../components/icons/TrophyIcon.tsx';
+import { User, Post } from './types.ts';
+import TrophyIcon from './components/icons/TrophyIcon.tsx';
 
 export interface Achievement {
   id: string;
@@ -19,7 +15,6 @@ export const ACHIEVEMENTS: Achievement[] = [
     id: 'first_post',
     title: 'Photographer',
     description: 'Make your first post.',
-    // FIX: Use React.createElement instead of JSX in a .ts file
     icon: React.createElement(TrophyIcon),
     isUnlocked: (user, data: { posts: Post[] }) => data?.posts.length >= 1,
   },
@@ -27,7 +22,6 @@ export const ACHIEVEMENTS: Achievement[] = [
     id: 'social_butterfly',
     title: 'Social Butterfly',
     description: 'Follow 10 people.',
-    // FIX: Use React.createElement instead of JSX in a .ts file
     icon: React.createElement(TrophyIcon),
     isUnlocked: (user) => user.following.length >= 10,
   },
@@ -35,7 +29,6 @@ export const ACHIEVEMENTS: Achievement[] = [
     id: 'popular',
     title: 'Popular',
     description: 'Get 25 followers.',
-    // FIX: Use React.createElement instead of JSX in a .ts file
     icon: React.createElement(TrophyIcon),
     isUnlocked: (user) => user.followers.length >= 25,
   },
@@ -45,7 +38,6 @@ export const ACHIEVEMENTS: Achievement[] = [
     description: 'Get your first match.',
     // This would require tracking match data, which is not in the current User model.
     // For now, this will be a placeholder.
-    // FIX: Use React.createElement instead of JSX in a .ts file
     icon: React.createElement(TrophyIcon),
     isUnlocked: (user) => false, 
   },
